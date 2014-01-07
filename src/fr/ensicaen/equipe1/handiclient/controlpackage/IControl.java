6a85ddc78d4d36385773870fc998c869900c0fbe
Lyoraen <lyoraen@gmail.com>
2014-01-07 19:09:26 +0100

    Modif Control

--- src/fr/ensicaen/equipe1/handiclient/controlpackage/IControl.java
+++ src/fr/ensicaen/equipe1/handiclient/controlpackage/IControl.java
@@ -1,5 +1,29 @@
 package fr.ensicaen.equipe1.handiclient.controlpackage;
 
-public interface IControl {
+import android.view.MotionEvent;
 
+public interface IControl {
+	public void useButton1();
+
+	public void useButton2();
+
+	public void useButton3();
+
+	public void useButton4();
+
+	public void useButton5();
+
+	public void useButton6();
+
+	public void useButton7();
+
+	public void useButton8();
+
+	public void useButton9();
+	
+	public void useButtonCancel();
+	
+	public void useButtonValidate();
+	
+	public void reactDependingOnUserActions(MotionEvent e);
 }
---
+++ src/fr/ensicaen/equipe1/handiclient/controlpackage/MultiTouchControl.java
@@ -0,0 +1,79 @@
+package fr.ensicaen.equipe1.handiclient.controlpackage;
+
+import android.view.MotionEvent;
+
+public class MultiTouchControl implements IControl {
+
+	@Override
+	public void useButton1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void useButton2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void useButton3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void useButton4() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void useButton5() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void useButton6() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void useButton7() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void useButton8() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void useButton9() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void useButtonCancel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void useButtonValidate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reactDependingOnUserActions(MotionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
