45184ac130b1a66607cadbb55102b210c3925d49
snifa <sanae.adrar@gmail.com>
2014-01-08 14:54:31 +0100

    modif multitouch

--- src/fr/ensicaen/equipe1/handiclient/controlpackage/IControl.java
+++ src/fr/ensicaen/equipe1/handiclient/controlpackage/IControl.java
@@ -3,23 +3,8 @@ package fr.ensicaen.equipe1.handiclient.controlpackage;
 import android.view.MotionEvent;
 
 public interface IControl {
-	public void useButton1();
 
-	public void useButton2();
-
-	public void useButton3();
-
-	public void useButton4();
-
-	public void useButton5();
-
-	public void useButton6();
-
-	public void useButton7();
-
-	public void useButton8();
-
-	public void useButton9();
+	public void useButton(int i);
 	
 	public void useButtonCancel();
 	
