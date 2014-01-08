a1cac20c881a60c4826502e2afd5f1016441c9a1
Lyoraen <lyoraen@gmail.com>
2014-01-08 17:32:05 +0100

    AudioView Modified

--- src/fr/ensicaen/equipe1/handiclient/authenticationpackage/AuthenticationView.java
+++ src/fr/ensicaen/equipe1/handiclient/authenticationpackage/AuthenticationView.java
@@ -3,20 +3,20 @@ package fr.ensicaen.equipe1.handiclient.authenticationpackage;
 import fr.ensicaen.equipe1.handiclient.viewpackage.IView;
 
 public class AuthenticationView {
-	
+
 	private AuthenticationActivity _authenticationActivity;
 	private IView _view;
-	
-	public AuthenticationView(AuthenticationActivity authenticationActivity,int viewType) {
+
+	public AuthenticationView(AuthenticationActivity authenticationActivity,
+			int viewType) {
 		_authenticationActivity = authenticationActivity;
-		switch(viewType)
-		{
+		switch (viewType) {
 		case 1:
-			_view = new AudioView();
+			_view = new AudioView(_authenticationActivity, _authenticationActivity.findViewById(id));
+			break;
+		default:
 			break;
 		}
 	}
-	
-	
 
 }
