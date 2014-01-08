9bf65eb7b407325520657ce30493fc7e6ab7255b
Lyoraen <lyoraen@gmail.com>
2014-01-08 10:46:58 +0100

    IHM

--- src/fr/ensicaen/equipe1/handiclient/homepackage/HomeActivity.java
+++ src/fr/ensicaen/equipe1/handiclient/homepackage/HomeActivity.java
@@ -1,11 +1,8 @@
 package fr.ensicaen.equipe1.handiclient.homepackage;
 
-import fr.ensicaen.equipe1.handiclient.R;
-import fr.ensicaen.equipe1.handiclient.R.layout;
-import fr.ensicaen.equipe1.handiclient.R.menu;
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import fr.ensicaen.equipe1.handiclient.R;
 
 public class HomeActivity extends Activity {
 
@@ -14,13 +11,4 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-    
 }
