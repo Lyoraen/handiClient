db6cca26193ca2f93cca6d2532f111bde96d661c
Quentin Rouault <quentin.rouault@ensicaen.fr>
2014-01-09 02:07:11 +0100

    Add of all the functions to contact the data base, but commented while not tested. Will be tested soon... maybe...

--- src/fr/ensicaen/equipe1/handiclient/networkpackage/NetworkHandler.java
+++ src/fr/ensicaen/equipe1/handiclient/networkpackage/NetworkHandler.java
@@ -1,33 +1,139 @@
 package fr.ensicaen.equipe1.handiclient.networkpackage;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public final class NetworkHandler {
 	private static NetworkHandler _instance = new NetworkHandler();
+	static final int serverPort = 6942;
+	static String serverName = "localhost";
 
 	private NetworkHandler() {
-		
+
 	}
 
 	public static NetworkHandler getInstance() {
 		return _instance;
 	}
-	
-	public boolean withdrawMoney(int money) {
+
+	public static void setServerName(String newName) {
+		serverName = newName;
+	}
+
+	public boolean withdrawMoney(String id, int money) {
+
+		/*Socket socket = new Socket(serverName, serverPort);
+		  ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+		  ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+		  out.flush();
+		  out.writeObject(id);
+		  out.writeObject(money);
+		  out.flush();
+
+		  boolean validation = (boolean)in.readObject();
+
+		  try {
+		  Thread.sleep(1000);
+		  } catch(InterruptedException ex) {
+		  Thread.currentThread().interrupt();
+		  }
+		  out.close();
+		  in.close();
+		  socket.close();
+
+		  return validation;
+		  */
+
 		return true;
 	}
-	
-	public boolean addUser(String name, int balance) {
-		return true;
+
+	public void addUser(String id, String name, int balance) {
+		/*Socket socket = new Socket(serverName, serverPort);
+		  ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+
+		  out.flush();
+		  out.writeObject(id);
+		  out.writeObject(name);
+		  out.writeObject(balance);
+		  out.flush();
+
+		  try {
+		  Thread.sleep(1000);
+		  } catch(InterruptedException ex) {
+		  Thread.currentThread().interrupt();
+		  }
+		  out.close();
+		  socket.close();
+
+		  return validation;*/
 	}
-	
-	public String getName() {
+
+	public String getName(String id) {
+		/*ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+		  ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+		  out.flush();
+		  out.writeObject(id);
+		  out.flush();
+
+		  String returnedName = (String)in.readObject();
+
+		  try {
+		  Thread.sleep(1000);
+		  } catch(InterruptedException ex) {
+		  Thread.currentThread().interrupt();
+		  }
+		  out.close();
+		  in.close();
+		  socket.close();
+
+		  return returnedName;*/
 		return "Name";
 	}
-	
-	public int getBalance() {
+
+	public int getBalance(String id) {
+		/*ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+		  ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+		  out.flush();
+		  out.writeObject(id);
+		  out.flush();
+
+		  double returnedBalance = (double)in.readObject();
+		  try {
+		  Thread.sleep(1000);
+		  } catch(InterruptedException ex) {
+		  Thread.currentThread().interrupt();
+		  }
+		  out.close();
+		  in.close();
+		  socket.close();
+
+		  return returnedBalance;*/
 		return 1;
 	}
 
-	public int getPersonalisation() {
-		return 0;
+	public boolean testUser(String id){
+		/*ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+		  ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+		  out.flush();
+		  out.writeObject(id);
+		  out.flush();
+
+		  boolean test = (boolean)in.readObject();
+		  try {
+		  Thread.sleep(1000);
+		  } catch(InterruptedException ex) {
+		  Thread.currentThread().interrupt();
+		  }
+		  out.close();
+		  in.close();
+		  socket.close();
+
+		  return test;*/
+		return true;
 	}
 }
