3790a09f1f3754539deceb272f536bbcb8b68173
dutche <aheinzle7@gmail.com>
2014-01-09 17:01:15 +0100

    menu m'en bat les couilles

--- src/fr/ensicaen/equipe1/handiclient/menupackage/MenuControl.java
+++ src/fr/ensicaen/equipe1/handiclient/menupackage/MenuControl.java
@@ -1,14 +1,49 @@
 package fr.ensicaen.equipe1.handiclient.menupackage;
 
 import fr.ensicaen.equipe1.handiclient.controlpackage.IControl;
+import fr.ensicaen.equipe1.handiclient.controlpackage.MultiTouchControl;
+import android.view.MotionEvent;
+import android.widget.Button;
 
-public class MenuControl {
-	
+public class MenuControl implements IControl {
+
 	private MenuActivity _menuActivity;
 	private IControl _control;
 
-	public MenuControl(MenuActivity menuActivity) {
+	public MenuControl(MenuActivity menuActivity, String controlType) {
 		_menuActivity = menuActivity;
+
+		if(controlType.equals("MULTITOUCH_MODE")) {
+			_control = new MultiTouchControl(this);
+		}
+	}
+
+	@Override
+	public void useButton(int i) {
+		switch(i) {
+		case 1 :
+			_menuActivity.intentToGoodByeActivity();
+		case 2 :
+			_menuActivity.intentWithdrawMoneyActivity();
+		case 3 :
+			_menuActivity.intentToGoodByeActivity();
+		}
+		_menuActivity.getView().reactOnNumberButtons((Button) _menuActivity.findViewById(_menuActivity.getResources().getIdentifier("menubutton"+i, "id", _menuActivity.getPackageName())));
+	}
+
+	@Override
+	public void useButtonCancel() {
+		_menuActivity.intentToGoodByeActivity();
+	}
+
+	@Override
+	public void reactDependingOnUserActions(MotionEvent e) {
+		_control.reactDependingOnUserActions(e);
+	}
+
+	@Override
+	public void useButtonValidate() {
+		// there is nothing in the fridge
 	}
 
 }
--- src/fr/ensicaen/equipe1/handiclient/menupackage/MenuView.java
+++ src/fr/ensicaen/equipe1/handiclient/menupackage/MenuView.java
@@ -1,16 +1,50 @@
 package fr.ensicaen.equipe1.handiclient.menupackage;
 
+import android.widget.Button;
+import fr.ensicaen.equipe1.handiclient.R;
+import fr.ensicaen.equipe1.handiclient.menupackage.MenuActivity;
+import fr.ensicaen.equipe1.handiclient.viewpackage.AudioView;
 import fr.ensicaen.equipe1.handiclient.viewpackage.IView;
 
-public class MenuView {
-	
+public class MenuView implements IView {
+
 	private MenuActivity _menuActivity;
 	private IView _view;
 
-	public MenuView(MenuActivity menuActivity) {
+	public MenuView(MenuActivity menuActivity,String viewType) {
 		_menuActivity = menuActivity;
+		if(viewType.equals("AUDIO_MODE")) {
+			_view = new AudioView(_menuActivity,R.id.layoutMenu);
+		}
+		/*
+		else {
+			break;
+		}
+		 */
 	}
-	
-	
 
+	public void describe() {
+		_view.describe();
+	}
+
+	@Override
+	public void reactOnNumberButtons(Button button) {
+		_view.reactOnNumberButtons(button);
+	}
+
+	@Override
+	public void reactOnCancelButton(Button button) {
+		_view.reactOnCancelButton(button);
+	}
+
+	@Override
+	public void reactOnValidateButton(Button button) {
+		_view.reactOnValidateButton(button);
+	}
+
+	@Override
+	public void describeActivity(String speech) {
+		// TODO Auto-generated method stub
+		
+	}
 }
